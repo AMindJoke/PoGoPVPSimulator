85dacad82074457be5bcbdda3428751d52c4b39b
AMindJoke <alelibero1993@gmail.com>
2026-09-07 17:16:24 +0200

    Refresh service worker for planner cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "pogo-pvp-simulator";
-const CACHE_VERSION = "2026-09-07-v25-cramorant";
+const CACHE_VERSION = "2026-09-07-v26-planner-refresh";
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VERSION}`;
 
 const CORE_ASSETS = [
